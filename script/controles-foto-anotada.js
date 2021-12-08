a0b58b7323ccc3040f278ef9178523c5cfd4ed6f
dissantos <disantosg18@gmail.com>
2021-12-08 13:53:48 -0300

    - Select image file from computer

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -4,3 +4,19 @@ let imagemEl  = document.querySelector('.foto-anotada > img');
 filtroSelectInput.addEventListener('change', () => {
     imagemEl.style.filter = filtroSelectInput.value;
 })
+
+let seletorArquivoInput = document.querySelector('#imagem');
+let reader = new FileReader();
+
+seletorArquivoInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if(file.type && file.type.startsWith('image/')){
+        reader.addEventListener('load', (e) => {
+            imagemEl.setAttribute('src',e.target.result);
+            imagemEl.setAttribute('alt', `Arquivo de imagem selecionado do computador: ${file.name}`);
+        })
+        reader.readAsDataURL(file);
+    } else {
+        console.error(`Arquivo ${file.name} não é uma imagem`);
+    }
+});
