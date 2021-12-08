957b7f69c811c526a28d46017435f24b2b21178b
dissantos <disantosg18@gmail.com>
2021-12-08 13:30:43 -0300

    - Controls change markings

--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -13,9 +13,10 @@ let tituloMarcadoInput = document.querySelector('#titulo-da-marcacao');
 let conteudoMarcadoInput = document.querySelector('#conteudo-da-marcacao');
 let corMarcadoInput = document.querySelector('#cor-da-marcacao');
 
-let formatoInput;
+let formatoStr = imagemMarcadaSelecionada.getAttribute('data-formato');
+let formatoInput = document.querySelector(`[value=${formatoStr}]`);
 
-function atualizarControles(el){
+function atualizarControles(el) {
     xMarcadoInput.value = parseFloat(el.style.left);
     yMarcadoInput.value = parseFloat(el.style.top);
     larguraMarcadoInput.value = parseFloat(el.style.width);
@@ -25,8 +26,8 @@ function atualizarControles(el){
     conteudoMarcadoInput.value = el.getAttribute('data-conteudo');
     corMarcadoInput.value = el.getAttribute('data-cor');
 
-    const formato = el.getAttribute('data-formato');
-    formatoInput = document.querySelector(`[value=${formato}]`)
+    formatoStr = el.getAttribute('data-formato');
+    formatoInput = document.querySelector(`[value=${formatoStr}]`);
     formatoInput.checked = true;
 
 }
@@ -52,4 +53,40 @@ imagemsMarcadasEl.forEach((imagemMarcadaEl) => {
     });
 });
 
+
+//modificar marcacao selecionada
+xMarcadoInput.addEventListener('change',() => {
+    imagemMarcadaSelecionada.style.left= `${parseFloat(xMarcadoInput.value)}px`;
+});
+yMarcadoInput.addEventListener('change',() => {
+    imagemMarcadaSelecionada.style.top= `${parseFloat(yMarcadoInput.value)}px`;
+});
+larguraMarcadoInput.addEventListener('change',() => {
+    imagemMarcadaSelecionada.style.width= `${parseFloat(larguraMarcadoInput.value)}px`;
+});
+alturaMarcadoInput.addEventListener('change',() => {
+    imagemMarcadaSelecionada.style.height= `${parseFloat(alturaMarcadoInput.value)}px`;
+});
+
+tituloMarcadoInput.addEventListener('change', () => {
+    imagemMarcadaSelecionada.setAttribute('data-titulo', tituloMarcadoInput.value);
+});
+conteudoMarcadoInput.addEventListener('change', () => {
+    imagemMarcadaSelecionada.setAttribute('data-conteudo', conteudoMarcadoInput.value);
+});
+corMarcadoInput.addEventListener('change', () => {
+    imagemMarcadaSelecionada.setAttribute('data-cor', corMarcadoInput.value);
+});
+
+let formatosMarcacaoInput = document.querySelectorAll('[name="formato-da-marcacao"]');
+formatosMarcacaoInput.forEach((formatoMarcado) => {
+    formatoMarcado.addEventListener('change', () => {
+        imagemMarcadaSelecionada.classList.remove(formatoStr);
+        if(formatoMarcado.checked){
+            imagemMarcadaSelecionada.setAttribute('data-formato',formatoMarcado.value);
+            imagemMarcadaSelecionada.classList.add(formatoMarcado.value);
+            formatoStr = formatoMarcado.value;
+        }
+    })
+})
 
